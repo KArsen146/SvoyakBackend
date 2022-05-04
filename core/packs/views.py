05f1ddccd1384664687ea1300f9043b6f0ce108e
DnC275 <275.denis@gmail.com>
2022-05-04 13:37:44 +0300

    + pagination for packs

--- core/packs/views.py
+++ core/packs/views.py
@@ -4,9 +4,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Pack
 from .serializers import PackSerializer
+from .pagination import PacksListPagination
 
 
 class PackViewSet(ModelViewSet):
     queryset = Pack.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = PackSerializer
+    pagination_class = PacksListPagination
