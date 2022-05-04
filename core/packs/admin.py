0287715ef55bce66ef5a838c11da8d5c05b8b0d3
DnC275 <275.denis@gmail.com>
2022-05-04 13:25:33 +0300

    + packs to admin

--- core/packs/admin.py
+++ core/packs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PackAdmin(admin.ModelAdmin):
+    fields = ('title', 'author')
+
+
+admin.site.register(Pack, PackAdmin)
